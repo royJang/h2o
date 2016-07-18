0a100156dbd4dc6ba73ac02e6da7b991d0ba6d99
royJang <zzl1108@hotmail.com>
2016-07-18 23:23:34 +0800

    update

--- src/components/new.vue
+++ src/components/new.vue
@@ -1,28 +1,31 @@
 <template>
 	<div class="panel panel-default">
 	  <div class="panel-heading">
-	  	<nav class="navbar navbar-default">
-		  <div class="container-fluid">
-		      <input type="text" class="form-control" />
-		  </div>
-		</nav>
+	  	<form class="navbar-form" style="padding:0;">
+		  	<select class="form-control">
+		  		<option>All</option>
+		  		<option>Error</option>
+		  	</select>
+		    <input type="text" class="form-control" placeholder="/api" />
+		</form>
   	  </div>
 	  <div class="panel-body">
 	    <ul class="list-group">
 			<li class="list-group-item" v-for="i in list" track-by="$index">
 				<span class="status status-200"></span>
-				200 
+				200 	
 				<span>/api/get/list</span>
+				<span class="pull-right doc-text">文檔</span>
 			</li>
 		</ul>
-	  </div>
+	  </div>	
 	</div>
 </template>
 <script type="text/javascript">
 	export default {
 		data (){
 			return {
-				list : new Array(10)
+				list : new Array(100)
 			};	
 		},
 		methods : {
@@ -31,13 +34,34 @@
 		}	
 	}
 </script>
-<style type="less">	
+<style lang="less">	
 	.status {
 		width:16px; height:16px;
 		border-radius:50%; display:inline-block;
 		vertical-align:-2px; margin-right:6px;
 	}
 	.status-200 {
-		background:linear-gradient(top,#34c72a,#299a21);
+		background:linear-gradient(top,#33ff00,#33ffcc);
+		animation:status200 4s infinite;
+	}
+	.doc-text {
+		color:#999;
+		&:hover {
+			text-decoration:underline; cursor: pointer;	
+		}	
+	}	
+	@keyframes status200 {
+		0% {
+			background:linear-gradient(top,#009966,#00CC00);
+		}
+		33% {
+			background:linear-gradient(right,#009900,#00CC66);
+		}
+		66% {
+			background:linear-gradient(bottom,#00CC66,#009900);
+		}
+		100% {
+			background:linear-gradient(left,#00CC00,#009966);
+		}
 	}
 </style>
